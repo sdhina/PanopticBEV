475b55882a78a891ca416f26724db1432787271e
sdhina <100837043+sdhina@users.noreply.github.com>
2023-05-27 19:53:36 +0200

    Update train_panoptic_bev_nuscenes.sh

--- scripts/train_panoptic_bev_nuscenes.sh
+++ scripts/train_panoptic_bev_nuscenes.sh
@@ -1,9 +1,11 @@
-CUDA_VISIBLE_DEVICES={CUDA GPU IDs} \
-python3 -m torch.distributed.launch --nproc_per_node={NUM GPUs} --master_addr={IP ADDR} --master_port={PORT NUM} train_panoptic_bev.py \
-                                    --run_name={NAME OF THE RUN} \
-                                    --project_root_dir={PATH OF THE panoptic_bev FOLDER} \
-                                    --seam_root_dir={PATH OF THE NUSCENES SEAMLESS DIRECTORY} \
-                                    --dataset_root_dir={PATH OF THE NUSCENES DATASET} \
+module load gcc/8.4.0-cuda python/3.7.7 cuda/11.1.1
+
+CUDA_VISIBLE_DEVICES=0 \
+../../venv/panoptic_bev/bin/python -m torch.distributed.launch --nproc_per_node=1 --master_addr=$(hostname -I | cut -d ' ' -f 1) --master_port=$(python /home/laubo/get_port.py) train_panoptic_bev.py \
+                                    --run_name=run1 \
+                                    --project_root_dir=/home/laubo/DLAV/PanopticBEV \
+                                    --seam_root_dir=/home/laubo/DLAV/nuscenes/nuScenes_panopticbev\
+                                    --dataset_root_dir=/work/scitas-share/datasets/Vita/civil-459/NuScenes_full/US \
                                     --mode=train \
                                     --train_dataset=nuScenes \
                                     --val_dataset=nuScenes \
