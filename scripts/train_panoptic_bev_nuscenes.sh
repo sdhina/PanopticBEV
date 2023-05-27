03729dc62e144788ec5e31ae7461602bc5fd2336
sdhinaaa <134807100+sdhinaaa@users.noreply.github.com>
2023-05-27 23:38:48 +0200

    Update train_panoptic_bev_nuscenes.sh

--- scripts/train_panoptic_bev_nuscenes.sh
+++ scripts/train_panoptic_bev_nuscenes.sh
@@ -1,7 +1,7 @@
 module load gcc/8.4.0-cuda python/3.7.7 cuda/11.1.1
 
 CUDA_VISIBLE_DEVICES=0 \
-../../venv/panoptic_bev/bin/python -m torch.distributed.launch --nproc_per_node=1 --master_addr=$(hostname -I | cut -d ' ' -f 1) --master_port=$(python /home/sdhina/get_port.py) train_panoptic_bev.py \
+../../venv/panoptic_bev/bin/python -m torch.distributed.launch --nproc_per_node=1 --master_addr=$(hostname -I | cut -d ' ' -f 1) --master_port=$(python /home/sdhina/TEMOR/PanopticBEV/scripts/get_port.py) train_panoptic_bev.py \
                                     --run_name=run1 \
                                     --project_root_dir=/home/sdhina/TEMOR/PanopticBEV \
                                     --seam_root_dir=/home/sdhina/TEMOR/nuscenes/nuScenes_panopticbev\
